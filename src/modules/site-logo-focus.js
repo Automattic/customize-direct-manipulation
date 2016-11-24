6fcf22569b6418f4038cff81282ff40ab592461f
Taegon Kim <gonom9@gmail.com>
2016-11-24 17:30:24 +0900

    Add support for Jetpack Site Logo
    See #13

---
+++ src/modules/site-logo-focus.js
@@ -0,0 +1,11 @@
+export function getSiteLogoElements() {
+	return [
+		{
+			id: 'site_logo',
+			selector: '.site-logo-link',
+			type: 'siteLogo',
+			icon: 'headerIcon',
+			title: 'site logo',
+		}
+	];
+}
--- src/preview.js
+++ src/preview.js
@@ -1,7 +1,7 @@
 import getWindow from './helpers/window';
 import getAPI from './helpers/api';
 import getJQuery from './helpers/jquery';
-import getOptions from './helpers/options';
+import getOptions, { isDisabled } from './helpers/options';
 import { isSafari, isMobileSafari } from './helpers/user-agent';
 import makeFocusable from './modules/focusable';
 import { modifyEditPostLinks, disableEditPostLinks } from './modules/edit-post-links';
@@ -9,6 +9,7 @@ import { getHeaderElements } from './modules/header-focus';
 import { getWidgetElements } from './modules/widget-focus';
 import { getMenuElements } from './modules/menu-focus';
 import { getFooterElements } from './modules/footer-focus';
+import { getSiteLogoElements } from './modules/site-logo-focus';
 
 const options = getOptions();
 const api = getAPI();
@@ -18,13 +19,14 @@ function startDirectManipulation() {
 	const basicElements = [
 		{ id: 'blogname', selector: '.site-title, #site-title', type: 'siteTitle', position: 'middle', title: 'site title' },
 	];
-	const headers = ( options.headerImageSupport ) ? getHeaderElements() : [];
-	const widgets = ( -1 === options.disabledModules.indexOf( 'widget-focus' ) ) ? getWidgetElements() : [];
-	const menus   = ( -1 === options.disabledModules.indexOf( 'menu-focus' ) )   ? getMenuElements() : [];
-	const footers = ( -1 === options.disabledModules.indexOf( 'footer-focus' ) ) ? getFooterElements() : [];
-	makeFocusable( basicElements.concat( headers, widgets, menus, footers ) );
+	const headers  = ! options.headerImageSupport    ? [] : getHeaderElements();
+	const widgets  = isDisabled( 'widget-focus' )    ? [] : getWidgetElements();
+	const menus    = isDisabled( 'menu-focus' )      ? [] : getMenuElements();
+	const footers  = isDisabled( 'footer-focus' )    ? [] : getFooterElements();
+	const siteLogo = isDisabled( 'site-logo-focus' ) ? [] : getSiteLogoElements();
+	makeFocusable( basicElements.concat( headers, widgets, menus, footers, siteLogo ) );
 
-	if ( -1 === options.disabledModules.indexOf( 'edit-post-links' ) ) {
+	if ( ! isDisabled( 'edit-post-links' ) ) {
 		if ( isSafari() && ! isMobileSafari() ) {
 			disableEditPostLinks( '.post-edit-link, [href^="https://wordpress.com/post"], [href^="https://wordpress.com/page"]' );
 		} else {
