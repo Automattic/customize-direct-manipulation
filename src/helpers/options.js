44a0c6c1ce87c5039dc4436e1ba991a4c9fd5455
Taegon Kim <gonom9@gmail.com>
2016-11-29 16:07:22 +0900

    Add support for Jetpack Site Logo (#41)
    See #13

--- src/helpers/options.js
+++ src/helpers/options.js
@@ -3,3 +3,7 @@ import getWindow from './window';
 export default function getOptions() {
 	return getWindow()._Customizer_DM;
 }
+
+export function isDisabled( moduleName ) {
+	return ( -1 !== getOptions().disabledModules.indexOf( moduleName ) );
+}
